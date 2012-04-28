4ba42e05f1a68e3d894601bd5087a5a31144ca6b
qin liu <qliu040@aucklanduni.ac.nz>
2012-04-28 19:41:05 +1200

    Update src/TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,9 +1,9 @@
-
 public class TestClass {
 	public void printSomething(){
 		System.out.print("hello");
 		System.out.print("world");
 		System.out.print("z60m");
+		System.out.print("mike");
 		
 	}
 }
