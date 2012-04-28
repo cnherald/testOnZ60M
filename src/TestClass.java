219d05753a94a65fe218bd0aad5ae011b1f71315
qin liu <qliu040@aucklanduni.ac.nz>
2012-04-28 20:23:50 +1200

    add line "System.out.print("mike");" on remote repo.

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,9 +1,8 @@
-
 public class TestClass {
 	public void printSomething(){
 		System.out.print("hello");
 		System.out.print("world");
 		System.out.print("z60m");		
-		
+		System.out.print("mike");
 	}
 }
