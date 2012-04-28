13108e9dd1e7af8b18520d4ff17c7b79885a9ba4
z60m <z60m@auckland.com>
2012-04-28 19:42:07 +1200

    nothing changed commit

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 	public void printSomething(){
 		System.out.print("hello");
 		System.out.print("world");
-		System.out.print("z60m");
+		System.out.print("z60m");		
 		
 	}
 }
